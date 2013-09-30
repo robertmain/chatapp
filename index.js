be00ca8db66f3e248485b94193ecf9101c866e63
Robert Main <lenswipe@gmail.com>
2013-10-01 00:38:28 +0100

    Updated web server to run on port 8000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 app.get("/", function(req, res){
     res.sendfile("page.htm");
 });
-var io = require('socket.io').listen(app.listen(8001));
+var io = require('socket.io').listen(app.listen(8000));
 io.sockets.on('connection', function(socket){
     socket.emit('message', {message: 'Welcome to the chat'});
     socket.broadcast.emit('message', {message: '<em>' + socket.handshake.address.address + ' Joined The Conversation</em>'});
