c42d9889ed4492678b15d88f9768f6609d55eabc
Robert Main <lenswipe@gmail.com>
2013-10-19 03:41:03 +0100

    Optional port argument

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var app = require('express')();
+var port = process.argv[2] || 8000;
 app.get("/", function(req, res){
     res.sendfile("page.htm");
 });
-var io = require('socket.io').listen(app.listen(8000));
+var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function(socket){
     socket.emit('message', {message: 'Welcome to the chat'});
     socket.broadcast.emit('message', {message: '<em>' + socket.handshake.address.address + ' Joined The Conversation</em>'});
